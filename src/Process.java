ab67ca2879dbb6ac693828b5253e75011814c01c
Dan <danbarry16@googlemail.com>
2020-12-04 21:31:06 +1300

    Basic processing class

---
+++ src/Process.java
@@ -0,0 +1,33 @@
+package b.ds;
+
+import java.net.Socket;
+
+/**
+ * Process.java
+ *
+ * This class is responsible for parsing the client and figuring out how it
+ * should be handled.
+ **/
+public class Process extends Thread{
+  /**
+   * Process()
+   *
+   * Allow the client to be processed.
+   *
+   * @param socket The socket of the client.
+   * @param recBuffSize The receiver buffer size.
+   **/
+  public Process(Socket socket, int recBuffSize){
+    /* TODO */
+  }
+
+  /**
+   * run()
+   *
+   * Start processing the user's request.
+   **/
+  @Override
+  public void run(){
+    /* TODO */
+  }
+}
