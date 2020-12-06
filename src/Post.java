1e771b53e02608073b24cff1cd1804e9c38811a1
Dan <danbarry16@googlemail.com>
2020-12-07 02:21:23 +1300

    Implement basic post reading from disk

--- src/Post.java
+++ src/Post.java
@@ -20,9 +20,39 @@ public class Post{
 
   /**
    * readPost()
+   *
+   * Read user post from disk and update the relevant variables. Return NULL if
+   * an issue occurs.
+   *
+   * @param path The path for the user configuration.
+   * @param post A post object to be written to.
+   * @return The post object, otherwise NULL.
    **/
-  public static Post readPost(String path){
-    return null; // TODO
+  public static Post readPost(String path, Post post){
+    try{
+      JSON postData = JSON.build(path);
+      post.id = postData.get("id").value(null);
+      post.userid = postData.get("userid").value(null);
+      try{
+        post.creation = Long.parseLong(postData.get("creation").value(".."));
+      }catch(NumberFormatException e){
+        post.creation = -1;
+      }
+      post.previous = postData.get("previous").value(null);
+      post.message = postData.get("message").value(null);
+      if(
+        post.id != null       &&
+        post.userid != null   &&
+        post.creation >= 0    &&
+        post.message != null
+      ){
+        return post;
+      }else{
+        return null;
+      }
+    }catch(Exception e){
+      return null;
+    }
   }
 
   /**
