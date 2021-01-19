89b7a1422218da19c9c3ab5edd8b8ffebe618a85
Dan <danbarry16@googlemail.com>
2021-01-19 18:59:44 +1300

    Generate front page based on latest posts

--- src/HandlerHome.java
+++ src/HandlerHome.java
@@ -1,5 +1,7 @@
 package b.ds;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 /**
@@ -21,9 +23,21 @@ public class HandlerHome extends Handler{
 
   @Override
   public byte[] genBody(){
-    /* TODO: Return default content here. */
-    return (
-      "Content here"
-    ).getBytes();
+    String res = "<h2>Latest posts</h2>";
+    /* Grab latest list of posts */
+    ArrayList<Post> posts = Post.getPosts();
+    /* Begin loading posts from most recent (last) */
+    for(int x = posts.size() - 1; x >= 0; x--){
+      Post post = posts.get(x);
+      Auth.User user = post.user;
+      res +=
+        "<p>" +
+          "<b><a href=\"/user/" + user.id + "\">@" + user.username +
+          "</a></b> on " + (new Date(post.creation)) + " said:" +
+          "<br>" +
+          "<quote>" + post.message + "</quote>" +
+        "</p>";
+    }
+    return res.getBytes();
   }
 }
