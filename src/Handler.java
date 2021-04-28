fe4f5e582be70a6caf97b0aff175d5a63d10fc17
Dan <danbarry16@googlemail.com>
2021-04-28 22:05:46 +1200

    Give the admin an admin link

--- src/Handler.java
+++ src/Handler.java
@@ -129,6 +129,10 @@ public abstract class Handler{
         .append(user.id.toString())
         .append("\">@")
         .append(user.username);
+      /* If an admin, add a link the admin panel */
+      if(user.role == Auth.Role.ADMIN){
+        res.append("</a> <a href=\"/about\">*");
+      }
     }
     res.append("</a></h1>");
     os.write(res.toByteArray());
