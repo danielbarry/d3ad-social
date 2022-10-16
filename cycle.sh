b1b36461734ee27e70c9c500f6cfe938207100c9
Dan <danbarry16@googlemail.com>
2022-10-16 01:56:41 +0100

    Always skip rebuild to limit server RAM usage

--- cycle.sh
+++ cycle.sh
@@ -32,7 +32,7 @@ do
     # Pull the latest changes
     git pull
     # Rebuild the files
-    ant
+#    ant
     # Restart the process
     restart_process
   else
@@ -42,7 +42,8 @@ do
   res="$(ps ax | grep $PROC | grep -v grep)"
   if [ "${res:-null}" = null ]; then
     # As it's not running, rebuild and restart it
-    ant && restart_process
+#    ant && restart_process
+    restart_process
     # Do another loop shortly
     sleep 30
   else
